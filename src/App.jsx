de797c09e049467d9e54d0fba9b5c08ca9b0d159
Lonath <lk.gcm29@gmail.com>
2024-07-05 12:38:45 +0530

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import './App.css'
 import Footer from './Footer.jsx'
 import Header from './Header.jsx'
 import ScrollContainer from './ScrollContainer.jsx'
+import HeroBackground from './HeroBackground.jsx'
+
 
 function App() {
 
   return (
     <>
     <Header></Header>
+    <HeroBackground />
     <ScrollContainer />
     <Footer></Footer>
     </>
